9140ac8a967faed3a7b4c7f171a6cea74a001c8c
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-10-14 13:35:41 -0300

    criando uma lista de alunos para testes.

--- SmartSchool/Controllers/AlunoController.cs
+++ SmartSchool/Controllers/AlunoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool.Models;
+using System.Collections.Generic;
 
 namespace SmartSchool.Controllers
 {
@@ -6,5 +8,32 @@ namespace SmartSchool.Controllers
 	[ApiController]
 	public class AlunoController : ControllerBase
 	{
+		public List<Aluno> Alunos = new List<Aluno>()
+		{
+			new Aluno()
+			{
+				Id = 1,
+				Name = "Daniel",
+				Sobrenome = "Oliveira",
+				Telefone = "912341234",
+				AlunoDisciplinas = null
+			},
+			new Aluno()
+			{
+				Id = 2,
+				Name = "Marion",
+				Sobrenome = "Defante",
+				Telefone = "988884444",
+				AlunoDisciplinas = null
+			},
+			new Aluno()
+			{
+				Id = 3,
+				Name = "Bartolomew",
+				Sobrenome = "Kuma",
+				Telefone = "932236446",
+				AlunoDisciplinas = null
+			}
+		};
 	}
 }
