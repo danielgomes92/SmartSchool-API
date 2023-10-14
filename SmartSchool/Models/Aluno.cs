776e60d2f89d4e864e0da3f29792dd03bf510eca
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-10-14 13:32:23 -0300

    definindo as propriedades na classe aluno

--- SmartSchool/Models/Aluno.cs
+++ SmartSchool/Models/Aluno.cs
@@ -1,6 +1,13 @@
-﻿namespace SmartSchool.Models
+﻿using System.Collections.Generic;
+
+namespace SmartSchool.Models
 {
 	public class Aluno
 	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Sobrenome { get; set; }
+		public string Telefone { get; set; }
+		public IEnumerable<AlunoDisciplina> AlunoDisciplinas { get; set; }
 	}
 }
