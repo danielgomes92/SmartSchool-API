65d0aca12ab86a73c2b80cdd8f795955248e4f74
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-10-23 03:05:37 -0300

    adicionando os pack nuggets e configurando o context

--- SmartSchool/Startup.cs
+++ SmartSchool/Startup.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SmartSchool.Data;
 
 namespace SmartSchool
 {
@@ -24,6 +20,8 @@ namespace SmartSchool
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<SmartContext>(
+				context => context.UseSqlite(Configuration.GetConnectionString("Default")));
 			services.AddControllers();
 		}
 
