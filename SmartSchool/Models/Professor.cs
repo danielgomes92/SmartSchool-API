77a0d513be0ae697aa36a4449d20383f13a321df
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-10-23 04:10:08 -0300

    classes e propriedades definidos.

--- SmartSchool/Models/Professor.cs
+++ SmartSchool/Models/Professor.cs
@@ -1,6 +1,20 @@
-﻿namespace SmartSchool.Models
+﻿using System.Collections.Generic;
+
+namespace SmartSchool.Models
 {
 	public class Professor
 	{
+		public Professor()
+		{
+		}
+		public Professor(int id, string nome)
+		{
+			Id = id;
+			Nome = nome;
+		}
+
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public IEnumerable<Disciplina> Disciplinas { get; set; }
 	}
 }
